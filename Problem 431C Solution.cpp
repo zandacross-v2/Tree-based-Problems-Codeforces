ec2a56726eea21144b96a8dd68b19154d2e971ba
Vardhan Malik <44138259+zandacross-v2@users.noreply.github.com>
2020-02-05 15:53:10 +0530

    Normal DP Tree problem with complexity of  O(nk)

---
+++ Problem 431C Solution.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ff first
+#define ss second
+#define pq priority_queue
+#define MOD 1000000007
+using namespace std;
+ 
+int main(){
+	ll n,k,d;
+	cin >> n >> k >> d;
+ 
+	ll dp[n+1][2];
+	dp[0][0]=1;
+	dp[0][1]=0;
+	for(int i=1;i<=n;i++){
+		dp[i][0]= dp[i][1]=0;
+		for(int j=1;j<=k;j++){
+			if(i-j<0){
+				break;
+			}
+			if(j<d){
+				dp[i][0] = (dp[i][0]%MOD +  dp[i-j][0]%MOD)%MOD;
+				dp[i][1] = (dp[i][1]%MOD + dp[i-j][1]%MOD)%MOD;
+			}
+			else{
+				dp[i][1] = (dp[i][1]%MOD +  dp[i-j][0]%MOD)%MOD;
+				dp[i][1] = (dp[i][1]%MOD + dp[i-j][1]%MOD)%MOD;
+			}
+		}
+	}
+	cout << dp[n][1] << endl;
+}
